ecf13edc6043aeafdee2825eaffb20da3a2c4d5e
Jorge Eliecer Soto Ramos <36306814+Jesr2104@users.noreply.github.com>
2022-08-26 00:35:04 +0100

    APIManager-Web Version 1.03

--- js/app.js
+++ js/app.js
@@ -1,11 +1,41 @@
-// vars
-const openModal = document.getElementById('openRegisterModal')
-const modal = document.getElementById('modal')
-const closeModal = document.getElementById('closeRegisterModal')
-
+// Functions
+//--------------------------------------------------------------------------
+// function to show the field to insert new product
 const showRegisterModal = () => {
     modal.classList.toggle('is-active')
 }
 
-openModal.addEventListener('click', showRegisterModal)
-closeModal.addEventListener('click', showRegisterModal)
+// Firebase Configuration
+async function configuration(){
+    const firebaseConfig = {
+        apiKey: "AIzaSyD38MhGW8-0NM4oRseY1xIrkI0Xj2-H464",
+        authDomain: "grocerystore-justjump.firebaseapp.com",
+        databaseURL: "https://grocerystore-justjump-default-rtdb.europe-west1.firebasedatabase.app",
+        projectId: "grocerystore-justjump",
+        storageBucket: "grocerystore-justjump.appspot.com",
+        messagingSenderId: "496229318920",
+        appId: "1:496229318920:web:98f64838f81f9744a32cbd"
+      };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig)
+}
+
+// vars
+//--------------------------------------------------------------------------
+const openModal = document.getElementById('openRegisterModal')
+const modal = document.getElementById('modal')
+const closeModal = document.getElementById('closeRegisterModal')
+const newProductForm = document.getElementById('newProduct-form')
+
+// call to the main function
+//--------------------------------------------------------------------------
+    // Events control -------------------->>>>
+    openModal.addEventListener('click', showRegisterModal)
+    closeModal.addEventListener('click', showRegisterModal)
+    newProductForm.addEventListener('submit', (e) => (
+        e.preventDefault()
+    ))
+
+    // Function call  -------------------->>>>
+    configuration()
+//--------------------------------------------------------------------------
