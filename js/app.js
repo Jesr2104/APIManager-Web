db1822de78514111745fa8ff2276f8c1b62d1c39
Jorge Eliecer Soto Ramos <36306814+Jesr2104@users.noreply.github.com>
2022-08-27 00:46:05 +0100

    APIManager-Web Version 1.04

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ const showRegisterModal = () => {
     modal.classList.toggle('is-active')
 }
 
-// Firebase Configuration
+// function to do Firebase Configuration
 async function configuration(){
     const firebaseConfig = {
         apiKey: "AIzaSyD38MhGW8-0NM4oRseY1xIrkI0Xj2-H464",
@@ -20,21 +20,99 @@ async function configuration(){
     firebase.initializeApp(firebaseConfig)
 }
 
+// function to check available
+function checkAvailable(){
+    return checkAvailableButton.checked;
+}
+
+// function to check Disable
+function checkDisable(){
+    return checkDisableButton.checked;
+}
+
+// function to check Season
+function checkSeason(){
+    return checkSeasonButton.checked;
+}
+
 // vars
 //--------------------------------------------------------------------------
+const endPointDB = 'NewProduct'
 const openModal = document.getElementById('openRegisterModal')
 const modal = document.getElementById('modal')
 const closeModal = document.getElementById('closeRegisterModal')
 const newProductForm = document.getElementById('newProduct-form')
+
+const checkAvailableButton = document.getElementById('isAvailableTrue')
+const checkDisableButton = document.getElementById('isDisableTrue')
+const checkSeasonButton = document.getElementById('inSeasonTrue')
 
 // call to the main function
 //--------------------------------------------------------------------------
     // Events control -------------------->>>>
     openModal.addEventListener('click', showRegisterModal)
     closeModal.addEventListener('click', showRegisterModal)
-    newProductForm.addEventListener('submit', (e) => (
+    newProductForm.addEventListener('submit', (e) => {
+
         e.preventDefault()
-    ))
+
+        // collect form information
+        const productName = document.getElementById("productName");
+        const origin = document.getElementById("origin");
+
+        const price = document.getElementById("price");
+        const MOQ = document.getElementById("MOQ");
+        const discount = document.getElementById("discount");
+
+        const category = document.getElementById("category");
+        const salesUnit = document.getElementById("salesUnit");
+
+        const isAvailable = document.getElementById("isAvailable");
+        const isDisable = document.getElementById("isDisable");
+        const inSeason = document.getElementById("inSeason");
+        
+        const description = document.getElementById("description");
+
+        // load image of the product
+        // const productImage = await upLoadImaProduct(getFile());          
+
+        // refence of the data base
+        const productRef = firebase.database().ref(endPointDB)
+        const postRef = productRef.push()
+
+        // check the mandatory fields -> Product name, origin, price, discount, category, salesUnit, productImage 
+        if(Boolean(productName.value) &&
+            Boolean(origin.value) &&
+            Boolean(price.value) &&
+            Boolean(discount.value) &&
+            !(category.value === "Select category") &&
+            !(salesUnit.value === "Select option") /* &&
+            Boolean(productImage.value)
+            */
+        ){
+            postRef.set({
+                idProduct: uuid.v4(),
+                productName: productName.value,
+                origin: origin.value,
+                price: price.value,
+                MOQ: MOQ.value,
+                discount: discount.value,
+                category: category.value,
+                salesUnit: salesUnit.value,
+                isAvailable: checkAvailable(),
+                isDisable: checkDisable(),
+                inSeason: checkSeason(),
+                description: description.value,
+            });
+    
+            alert("Successfully inserted")
+            showRegisterModal()        
+        } 
+        else
+        {
+            alert('Any of the required fields are empty!!')
+        }
+    })
 
     // Function call  -------------------->>>>
     configuration()
